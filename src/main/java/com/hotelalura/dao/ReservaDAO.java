44c4c432ab44f4a850651b48bc1568a39a5c4cf3
Jorge Villafuerte <jorge_123_386@hotmail.es>
2023-09-13 15:02:19 -0400

    Added the methods editar, guardar, close and getMessage, at the same time show detailed information about the exception

--- src/main/java/com/hotelalura/dao/ReservaDAO.java
+++ src/main/java/com/hotelalura/dao/ReservaDAO.java
@@ -2,6 +2,7 @@ package com.hotelalura.dao;
 
 import com.hotelalura.model.Reserva;
 
+import javax.swing.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,6 +12,7 @@ import java.util.List;
 
 public class ReservaDAO {
     private final Connection connection;
+    private String message;
 
     public ReservaDAO(Connection connection) {
         this.connection = connection;
@@ -34,10 +36,76 @@ public class ReservaDAO {
                             resultSet.getString("forma_de_pago")
                     ));
                 }
+                resultSet.close();
             }
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            System.out.println(e.getMessage());
         }
         return listaReservas;
+    }
+
+    public void editar(Integer id, String fechaIn, String fechaOut, Double valor, String tipoPago) {
+        try {
+            final PreparedStatement queryUpdate = connection.prepareStatement(
+                    "UPDATE reserva SET " +
+                            "fecha_entrada = ?, " +
+                            "fecha_salida = ?, " +
+                            "valor = ?, " +
+                            "forma_de_pago = ? " +
+                            "WHERE id = ? "
+            );
+            try (queryUpdate) {
+                queryUpdate.setString(1, fechaIn);
+                queryUpdate.setString(2, fechaOut);
+                queryUpdate.setDouble(3, valor);
+                queryUpdate.setString(4, tipoPago);
+                queryUpdate.setInt(5, id);
+                queryUpdate.execute();
+
+                JOptionPane.showMessageDialog(null, "Reserva modificada con éxito.");
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, String.format("Error: %s", e.getMessage()));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Integer guardar(Integer id, String fechaEntrada, String fechaSalida, Double valor, String formaPago) {
+        try {
+            PreparedStatement queryInsert = connection.prepareStatement(
+                    "INSERT INTO reserva " +
+                            "(id, fecha_entrada, fecha_salida, valor, forma_de_pago) " +
+                            "VALUES (?, ?, ?, ?, ?)"
+            );
+            try (queryInsert) {
+                queryInsert.setInt(1, id);
+                queryInsert.setString(2, fechaEntrada);
+                queryInsert.setString(3, fechaSalida);
+                queryInsert.setDouble(4, valor);
+                queryInsert.setString(5, formaPago);
+                queryInsert.execute();
+                return queryInsert.getUpdateCount();
+            } catch (Exception e) {
+                this.message = e.getMessage();
+                return 10;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
+
+    public void close() {
+        try {
+            this.connection.close();
+            System.out.println("ReservaDAO cerró su conexión a la base de datos");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getStackTrace(), e.getMessage(),JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public String getMessage() {
+        return this.message;
     }
 }
